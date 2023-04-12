ee6687180dd8b03ef88a6d58ca2a06c6e9f89acc
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-04-13 01:59:50 +0200

    working on the cars

--- car_manager.py
+++ car_manager.py
@@ -1,3 +1,4 @@
+import random
 from turtle import Turtle
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
@@ -7,4 +8,6 @@ MOVE_INCREMENT = 10
 class CarManager(Turtle):
     def __init__(self):
         super().__init__()
+        self.color(random.choice(COLORS))
+        self.forward(STARTING_MOVE_DISTANCE)
 
--- main.py
+++ main.py
@@ -4,20 +4,24 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
+player = Player()
+car_manager = CarManager()
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
-
-player = Player()
-
 screen.onkey(player.up, "Up")
-
 screen.listen()
+
+
+
+
+
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
-    screen.tracer(1)
+    screen.update()
 
 
 screen.exitonclick()
