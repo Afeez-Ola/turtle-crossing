418635bfda6899cf610f5ff535d7f17050d44efb
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-04-12 16:19:49 +0200

    Initialised the turtle in the classes.

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ screen.tracer(0)
 
 player = Player()
 car_manager = CarManager()
-scoreboard=  Scoreboard()
+scoreboard = Scoreboard()
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
