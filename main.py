e3d3960c99beca2e79b5b90848c0977b041735da
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-04-13 01:54:56 +0200

    Initialised the turtle in the classes.

--- main.py
+++ main.py
@@ -9,11 +9,13 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
-car_manager = CarManager()
-scoreboard = Scoreboard()
+
+screen.onkey(player.go_up, "Up")
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
-    screen.update()
+    screen.tracer(1)
 
+
+screen.exitonclick()
--- player.py
+++ player.py
@@ -7,4 +7,12 @@ FINISH_LINE_Y = 280
 class Player(Turtle):
     def __init__(self):
         super().__init__()
-        self.color("green")
+        self.color("black")
+        self.shape("turtle")
+        self.penup()
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
+
+    def go_up(self):
+        new_y = self.ycor() + MOVE_DISTANCE
+        self.setposition(self.xcor(), new_y)
