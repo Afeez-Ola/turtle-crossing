07380c8c23da045317778478049b17970316c56e
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-04-13 02:51:26 +0200

    working on the cars

--- car_manager.py
+++ car_manager.py
@@ -6,6 +6,7 @@ STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 RANDOM_POSITION = random.randint(0, 280)
 
+
 class CarManager(Turtle):
     def __init__(self):
         super().__init__()
@@ -13,15 +14,26 @@ class CarManager(Turtle):
         self.penup()
         self.setposition(270, RANDOM_POSITION)
         self.color(random.choice(COLORS))
+        self.shapesize(stretch_wid=2, stretch_len=1, outline=1)
+        self.setheading(90)
+        self.forward(STARTING_MOVE_DISTANCE)
+        self.create_new_car()
 
+    def create_new_car(self):
+        for i in range(5):
+            self.setposition(270, RANDOM_POSITION)
+            print(RANDOM_POSITION)
+
+        self.shape("square")
+        self.penup()
+        self.color(random.choice(COLORS))
+        self.forward(STARTING_MOVE_DISTANCE)
         self.shapesize(stretch_wid=2, stretch_len=1, outline=1)
         self.setheading(90)
 
-
     def car_move(self):
-        self.forward(STARTING_MOVE_DISTANCE)
-        new_x = self.xcor() - 10
-        self.goto(new_x, RANDOM_POSITION)
-
-
-
+        for _ in range(10):
+            self.create_new_car()
+            self.forward(STARTING_MOVE_DISTANCE)
+            new_x = self.xcor() - 10
+            self.goto(new_x, RANDOM_POSITION)
