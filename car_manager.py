5e85952b954d8f7b11d5579cbff6f25899f3bf65
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-04-13 08:43:13 +0200

    Refactoring

--- car_manager.py
+++ car_manager.py
@@ -2,31 +2,29 @@ import random
 from turtle import Turtle
 
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_POSITION = (270, 0)
+MOVE_DISTANCE = 10
+MOVE_INCREMENT = 5
 
 
 class CarManager:
     def __init__(self):
-
         self.all_cars = []
+        self.car_speed = MOVE_DISTANCE
 
-    def create_new_car(self, ):
+    def create_new_car(self):
         num_of_cars = random.randint(1, 6)
         if num_of_cars == 1:
             car = Turtle("square")
-
             car.penup()
-            car.hideturtle()
-            car.setheading(180)
-            car.goto(270, 0)
             car.color(random.choice(COLORS))
-            car.shapesize(stretch_wid=1, stretch_len=2, outline=1)
             RANDOM_POSITION = random.randint(-250, 280)
-            car.setposition(300, RANDOM_POSITION)
-            car.showturtle()
+            car.goto(300, RANDOM_POSITION)
             self.all_cars.append(car)
-            return car
 
-    def car_move(self, speed):
+    def move_cars(self):
         for car in self.all_cars:
-            car.forward(speed)
+            car.backward(self.car_speed)
 
+    def level_up(self):
+        self.car_speed += MOVE_INCREMENT
